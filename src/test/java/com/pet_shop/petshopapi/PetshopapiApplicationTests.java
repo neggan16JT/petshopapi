51fc93baefd33c5dab54edd1fc04fca3eb20dfa3
neggan16JT <tobarocampojuanmanuel4@gmail.com>
2024-08-10 15:23:07 -0500

    primer comit

---
+++ src/test/java/com/pet_shop/petshopapi/PetshopapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet_shop.petshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
